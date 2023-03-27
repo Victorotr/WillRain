76b53f0b6a58f8d8e248c06b3fa53eb9e1073795
luisja.rodriguez <luisjaroma@gmail.com>
2023-03-27 13:41:44 +0200

    llovera

--- index.js
+++ index.js
@@ -5,61 +5,37 @@ const buttonSearch = document.querySelector("#search");
 const liElement = document.querySelector(".main");
 const temperatureElement = document.querySelector(".temp");
 
-const APIKey = "2b639106cec3da54fdacf42719dbbcea";
+const APIKey = "ff1890d6b5c6ab10a7a079f29d86c7eb";
 buttonGeolocation.addEventListener("click", () => {
   navigator.geolocation.getCurrentPosition((position) => {
     const { latitude, longitude } = position.coords;
 
     fetch(
-      `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude={part}&appid=${APIKey}`
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${APIKey}`
     )
       .then((response) => response.json())
       .then((dataWeather) => {
-        // const arrayRain = [];
-
-        // for (const date in dataWeather.hourly) {
-        //   const hourWeather = dataWeather.hourly[date].weather;
-
-        //   for (const hourDate in hourWeather) {
-        //     if (date > 7) {
-        //       break;
-        //     }
-        //     if (hourWeather[hourDate].main === "Rain") {
-        //       arrayRain.push(hourWeather[hourDate].main);
-        //     }
-        //   }
-        // }
-
-        // if (arrayRain.length > 0) {
+        console.log(dataWeather);
+        // const forecast = dataWeather.hourly.slice(0, 8);
+        // const willRain = forecast.some(
+        //   (hour) => hour.weather[0].main.toLowerCase() === "rain" // Probabilidad de lluvia??
+        // );
+        // if (willRain) {
         //   liElement.textContent += " Si";
         // } else {
         //   liElement.textContent += " No";
         // }
-        console.log(dataWeather);
-        const forecast = dataWeather.hourly.slice(0, 8);
-        const willRain = forecast.some(
-          (hour) => hour.weather[0].main.toLowerCase() === "rain" // Probabilidad de lluvia??
-        );
-        if (willRain) {
-          liElement.textContent += " Si";
-        } else {
-          liElement.textContent += " No";
-        }
-        const temp = dataWeather.daily[0].temp.day;
-        temperatureElement.textContent += ` ${temp}`;
+        // const temp = dataWeather.daily[0].temp.day;
+        // temperatureElement.textContent += ` ${temp}`;
       });
   });
 });
 
 buttonSearch.addEventListener("click", () => {
-  // const APIKey = "ff1890d6b5c6ab10a7a079f29d86c7eb";
   const city = document.querySelector("#search-input").value;
-  // const cnt = 8;
 
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`
-    // `https://pro.openweathermap.org/data/2.5/forecast/hourly?q=${city}&appid=${APIKey}`
-    // `https://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&cnt=${cnt}&appid=${APIKey}`
+    `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}`
   )
     .then((response) => response.json())
     .then((dataWeather) => {
