273d3e6003730f8f8fe9da01683cabdc0c61fd04
luisja.rodriguez <luisjaroma@gmail.com>
2023-03-28 10:30:03 +0200

    llovera

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 
 // Selectores
 
-const buttonGeolocation = document.querySelector("#find-me");
-const buttonSearch = document.querySelector("#search");
+const buttonGeolocation = document.querySelector("#geolocation-button");
+const buttonSearch = document.querySelector("#search-button");
 const descriptionElement = document.querySelector(".description");
-const temperatureElement = document.querySelector(".temp");
+const temperatureElement = document.querySelector(".temperature");
 const humidityElement = document.querySelector(".humidity");
-const windElement = document.querySelector(".wind_speed");
-const liElement = document.querySelector(".main");
+const windElement = document.querySelector(".wind-speed");
+const rainElement = document.querySelector(".rain");
 
 // ApiKey
 
@@ -40,9 +40,9 @@ buttonGeolocation.addEventListener("click", () => {
       );
 
       if (willRain) {
-        liElement.textContent += " Si";
+        rainElement.textContent += " Si";
       } else {
-        liElement.textContent += " No";
+        rainElement.textContent += " No";
       }
 
       const descrip = dataWeather.list[0].weather[0].description;
@@ -64,7 +64,7 @@ buttonGeolocation.addEventListener("click", () => {
     temperatureElement.innerHTML = "Temperature:";
     humidityElement.innerHTML = "Humidity:";
     windElement.innerHTML = "Wind Speed:";
-    liElement.innerHTML = "¿Lloverá en las próximas 8 horas?";
+    rainElement.innerHTML = "Will rain?";
   });
 });
 
@@ -93,9 +93,9 @@ buttonSearch.addEventListener("click", () => {
     );
 
     if (willRain) {
-      liElement.textContent += " Si";
+      rainElement.textContent += " Si";
     } else {
-      liElement.textContent += " No";
+      rainElement.textContent += " No";
     }
 
     const descrip = dataWeather.list[0].weather[0].description;
@@ -117,60 +117,5 @@ buttonSearch.addEventListener("click", () => {
   temperatureElement.innerHTML = "Temperature:";
   humidityElement.innerHTML = "Humidity:";
   windElement.innerHTML = "Wind Speed:";
-  liElement.innerHTML = "¿Lloverá en las próximas 8 horas?";
+  rainElement.innerHTML = "Will rain?";
 });
-
-// Presionar enter
-
-// buttonSearch.addEventListener("keyup", function (event) {
-//   if (event.keyCode === 13) {
-//     buttonSearch.click();
-//   const city = document.querySelector("#search-input").value;
-//   const urlCity = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}`;
-//   async function getWeather() {
-//     try {
-//       let response = await fetch(urlCity);
-//       let data = await response.json();
-//       return data;
-//     } catch (error) {
-//       console.error("ERROR:", error);
-//     }
-//   }
-
-//   async function weather() {
-//     const dataWeather = await getWeather();
-//     console.log(dataWeather);
-
-//     const forecast = dataWeather.list.slice(0, 8);
-//     const willRain = forecast.some(
-//       (hour) => hour.weather[0].main.toLowerCase() === "rain"
-//     );
-
-//     if (willRain) {
-//       liElement.textContent += " Si";
-//     } else {
-//       liElement.textContent += " No";
-//     }
-
-//     const descrip = dataWeather.list[0].weather[0].description;
-//     const temp = dataWeather.list[0].main.temp;
-//     const conversionKelvin = 273.15;
-//     const humidity = dataWeather.list[0].main.humidity;
-//     const wind = dataWeather.list[0].wind.speed;
-
-//     descriptionElement.textContent += descrip;
-//     temperatureElement.textContent += ` ${parseInt(
-//       temp - conversionKelvin
-//     )} ºC`;
-//     humidityElement.textContent += ` ${parseInt(humidity)} %`;
-//     windElement.textContent += ` ${parseInt(wind)} km/h`;
-//   }
-//   weather();
-
-//   descriptionElement.innerHTML = "";
-//   temperatureElement.innerHTML = "Temperature:";
-//   humidityElement.innerHTML = "Humidity:";
-//   windElement.innerHTML = "Wind Speed:";
-//   liElement.innerHTML = "¿Lloverá en las próximas 8 horas?";
-//   }
-// });
